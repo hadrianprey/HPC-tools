8e09c9c784f23105e2414d6304ddaf3a63b85a38
javier-darriba <javier.darriba@udc.es>
2023-12-26 13:47:58 +0100

    Final auto-vectorize vesion for GCC

--- dgesv.c
+++ dgesv.c
@@ -1,13 +1,8 @@
 #include "dgesv.h"
-int N = 4;
 
-int my_dgesv(int n, int nrhs, double *a, double *b)
+int my_dgesv(int n, int nrhs, double *restrict a, double *restrict b)
 {
     int i, j, k;
-
-    double* ax = (double*) __builtin_assume_aligned(a, N);
-    double* bx = (double*) __builtin_assume_aligned(b, N);
-
 
     for (k = 0; k < n; k++) {
         double pivot = a[k * n + k];
@@ -18,41 +13,47 @@ int my_dgesv(int n, int nrhs, double *a, double *b)
         }
 
         // Escalamos la fila k para que el pivote sea igual a 1
-        #pragma omp simd aligned(ax:N)
+        #pragma omp simd
         for (j = k; j < n; j++) {
-            ax[k * n + j] /= pivot;
+            double tmp = a[k * n + j] / pivot;
+            a[k * n + j] = tmp;
         }
 
         // Escalamos el lado derecho del sistema
-        #pragma omp simd aligned(bx:N)
+        #pragma omp simd
         for (j = 0; j < nrhs; j++) {
-            bx[k * nrhs + j] /= pivot;
+            double tmp = b[k * nrhs + j] / pivot;
+            b[k * nrhs + j] = tmp;
         }
 
         // Eliminación gaussiana para hacer ceros por debajo del pivote
         for (i = k + 1; i < n; i++) {
-            double factor = ax[i * n + k];
+            double factor = a[i * n + k];
 
             // Actualizamos la fila i restando el múltiplo del pivote
-            #pragma omp simd aligned(ax:N)
+            #pragma omp simd
             for (j = k; j < n; j++) {
-                ax[i * n + j] -= factor * ax[k * n + j];
+            double tmp = a[i * n + j] - factor * a[k * n + j];
+            a[i * n + j] = tmp;
             }
 
             // Actualizamos el lado derecho del sistema
-            #pragma omp simd aligned(bx:N)
+            #pragma omp simd
             for (j = 0; j < nrhs; j++) {
-                bx[i * nrhs + j] -= factor * bx[k * nrhs + j];
+                double tmp = b[i * nrhs + j] - factor * b[k * nrhs + j];
+                b[i * nrhs + j] = tmp;
             }
         }
     }
 
     // Sustitución hacia atrás para encontrar la solución
+    #pragma omp simd
     for (i = n - 2; i >= 0; i--) {
         for (j = i + 1; j < n; j++) {
-            #pragma omp simd aligned(bx:N)
-            for (k = 0; k < nrhs; k++) {
-                bx[i * nrhs + k] -= a[i * n + j] * bx[j * nrhs + k];
+            #pragma omp simd
+            for (int k = 0; k < nrhs; k++) {
+                double tmp = b[i * nrhs + k] - a[i * n + j] * b[j * nrhs + k];
+                b[i * nrhs + k] = tmp;
             }
         }
     }
